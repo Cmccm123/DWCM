b4aab0b10786d261cffd996bd29ecede271706c3
Sunny <Cmccm123@gmail.com>
2021-11-12 22:19:20 +0800

    first commit

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ from setuptools import setup
 with open('package.json') as f:
     package = json.load(f)
 
+with open(os.path.dirname(os.path.abspath(__file__))+"\\README.txt","r",encoding='utf-8') as fh:
+  long_description = fh.read()
 package_name = package["name"].replace(" ", "_").replace("-", "_")
 
 setup(
@@ -18,6 +20,8 @@ setup(
     description=package.get('description', package_name),
     install_requires=['jupyter_dash','dash_html_components','dash'],
     keywords = ['D3', 'D3.js', 'Word Cloud','DASH','data visualization'],
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     classifiers = [
         'Framework :: Dash',
     ],    
